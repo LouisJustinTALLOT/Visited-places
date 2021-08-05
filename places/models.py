559aa646f56269a10be47a108b50e3b0d6dfc5d5
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-08-05 21:30:32 +0200

    Adding the Place model
    The Place model allows us to store informations about a
    previously visited place, along with its spatial coordinates.

--- places/models.py
+++ places/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.gis.db import models as gis_models
 
 # Create your models here.
+class Place(models.Model):
+    """Model storing a location that has been visited"""
+
+    name = models.TextField(verbose_name="Nom du lieu")
+    date_visited = models.DateField(verbose_name="Date de dernière visite")
+
+    coordinates = gis_models.PointField(verbose_name="Coordonnées", null=True, blank=True)
