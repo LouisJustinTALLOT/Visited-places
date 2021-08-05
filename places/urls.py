5eee6e874c6fb8f2b5a404f11be6bddb33a44b63
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-08-06 01:08:48 +0200

    First view simply showing a map centered on Europe

--- places/urls.py
+++ places/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('all_visited_places/', views.all_visited_places, name='all_visited_places'),
 ]
--- places/views.py
+++ places/views.py
@@ -1,9 +1,16 @@
+# Django methods and classes
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
 
+# Modules for geographical data manipulation
+import folium
 
 def index(request):
     return HttpResponse("Hello, world. You're at the places index.")
+
+def all_visited_places(request):
+    m = folium.Map(location=[48.844952, 2.339193], zoom_start=4)
+
+    final_map = m._repr_html_()
+
+    return render(request, 'places/all_visited_places.html', locals())
