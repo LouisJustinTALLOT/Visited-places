3289c1d3fbc69e8c724a69d8cb5416e614f848fb
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-08-06 01:13:35 +0200

    Add pins with all the places visited to the map

--- places/views.py
+++ places/views.py
@@ -5,12 +5,24 @@ from django.http import HttpResponse
 # Modules for geographical data manipulation
 import folium
 
+# My models
+from .models import Place
+
 def index(request):
     return HttpResponse("Hello, world. You're at the places index.")
 
 def all_visited_places(request):
+    # create a map
     m = folium.Map(location=[48.844952, 2.339193], zoom_start=4)
 
-    final_map = m._repr_html_()
+    # get all Places objects
+    places = Place.objects.all()
+    for place in places:
+        # add a marker to the map
+        long, lat = place.coordinates.coords
+        print(long, lat)
+        folium.Marker([lat, long], popup=place.name).add_to(m)
 
+    # return the map
+    final_map = m._repr_html_()
     return render(request, 'places/all_visited_places.html', locals())
