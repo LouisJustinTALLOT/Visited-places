990b428211450a6f700822d98e1a806bfc6970f0
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-08-24 22:04:58 +0200

    Simplify new place form

--- places/forms.py
+++ places/forms.py
@@ -1,7 +1,18 @@
 from django.contrib.gis import forms
+from django.contrib.gis.db import models as gis_models
+from django.contrib.gis.forms import ModelForm
 
-class PlaceForm(forms.Form):
-    name = forms.CharField(label="Nom du lieu")
-    date_visited = forms.DateField(label="Date de dernière visite")
+from .models import Place
 
-    # coordinates = forms.PointField(label="Coordonnées")
+# class PlaceForm(forms.Form):
+#     name = forms.CharField(label="Nom du lieu")
+#     date_visited = forms.DateField(label="Date de dernière visite")
+
+#     # coordinates = gis_models.PointField(verbose_name="Coordonnées", geography=True)
+#     coordinates = gis_models.PointField(verbose_name="Coordonnées", geography=True, widget= forms.OSMWidget(attrs={'map_width': 800, 'map_height': 500}))
+
+class PlaceForm(forms.ModelForm):
+    
+    class Meta:
+        model = Place
+        fields = ("name","date_visited", "coordinates",)
