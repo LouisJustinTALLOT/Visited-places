d00736e45f1373f2c7b48a07fec2cdfeebf8e803
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-09-07 16:12:09 +0200

    Add visited places around the main stay location

--- places/forms.py
+++ places/forms.py
@@ -7,15 +7,19 @@ from django.contrib.gis.forms import ModelForm
 from .models import Stay
 
 
+class MultiPointWidget(floppyforms.gis.MultiPointWidget, floppyforms.gis.BaseOsmWidget):
+    pass
+
 class PointWidget(floppyforms.gis.PointWidget, floppyforms.gis.BaseOsmWidget):
     pass
 
 class PlaceForm(floppyforms.ModelForm):
     class Meta:
         model = Stay
-        fields = ("name","date_visited", "coordinates",)
+        fields = ("name","date_visited", "coordinates", "visits",)
         widgets = {
             "name": floppyforms.TextInput(attrs={"placeholder": "Nom du lieu"}),
             "date_visited": floppyforms.DateInput(attrs={"type": "date"}),
             "coordinates": PointWidget(),
+            "visits": MultiPointWidget(),
         }
--- places/models.py
+++ places/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.gis.db import models as gis_models
 
-# Create your models here.
 class Stay(models.Model):
     """Model storing a place that has been travelled to"""
 
     name = models.TextField(verbose_name="Lieu du voyage")
-    date_visited = models.DateField(verbose_name="Date de dernière visite")
+    date_visited = models.DateField(verbose_name="Date de dernière visite", blank=True)
 
     coordinates = gis_models.PointField(verbose_name="Coordonnées", null=True, blank=True)
+
+    visits = gis_models.MultiPointField(verbose_name="Lieux visités", null=True, blank=True)
 
     class Meta:
         verbose_name = "Voyage"
